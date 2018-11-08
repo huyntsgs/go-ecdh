8976b96eab2cb9da0a3649492786297ebb7b0adc
HuyNT <huykbc@gmail.com>
2018-11-08 21:22:48 +0700

    Add marshal and unmarshal private key

--- ecdh.go
+++ ecdh.go
@@ -9,6 +9,8 @@ import (
 type ECDH interface {
 	GenerateKey(io.Reader) (crypto.PrivateKey, crypto.PublicKey, error)
 	Marshal(crypto.PublicKey) []byte
+	MarshalPrivateKey(crypto.PrivateKey) []byte
 	Unmarshal([]byte) (crypto.PublicKey, bool)
+	UnmarshalPrivateKey(data []byte) crypto.PrivateKey
 	GenerateSharedSecret(crypto.PrivateKey, crypto.PublicKey) ([]byte, error)
 }
